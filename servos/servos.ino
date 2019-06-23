6da134a7152c4427a993eed1d3d15812ac3a1127
Rubén Soler <ruben.soler@exp360.com>
2019-06-23 23:14:01 +0200

    Feature ultrasonic sensor HC-SR04 distance

--- servos/servos.ino
+++ servos/servos.ino
@@ -6,13 +6,15 @@ int PINSERVO = 2;
 int PULSOMIN = 500;
 int PULSOMAX = 2400;
 
-void setup() {
+void setup()
+{
    servo.attach(PINSERVO, PULSOMIN, PULSOMAX);
 }
 
-void loop() {
- servo.write(0);
-  delay(2000);
-  servo.write(180);
-  delay(2000);
+void loop()
+{
+   servo.write(0);
+   delay(2000);
+   servo.write(180);
+   delay(2000);
 }
---
+++ ultrasonic-sensor/ultrasonic-sensor.ino
@@ -0,0 +1,27 @@
+int TRIG = 10;
+int ECO = 9;
+int LED = 2;
+
+int DURATION;
+int DISTANCE;
+
+void setup()
+{
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECO, INPUT);
+  pinMode(LED, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  digitalWrite(TRIG, HIGH);
+  delay(1);
+  digitalWrite(TRIG, LOW);
+  DURATION = pulseIn(ECO, HIGH);
+  DISTANCE = DURATION / 58.2; // CONSTANT OF MY SENSOR
+  Serial.println(DISTANCE);
+  delay(200);
+
+
+}
