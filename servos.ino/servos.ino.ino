17b0ddd60f7b4fc55f99665ed6c4e34004bb764c
Rubén Soler <ruben.soler@exp360.com>
2019-06-23 22:17:26 +0200

    Feature servo

---
+++ servos.ino/servos.ino.ino
@@ -0,0 +1,18 @@
+#include <Servo.h>
+
+Servo servo;
+
+int PINSERVO = 2;
+int PULSOMIN = 500;
+int PULSOMAX = 2400;
+
+void setup() {
+   servo.attach(PINSERVO, PULSOMIN, PULSOMAX);
+}
+
+void loop() {
+  servo.write(0);
+  delay(2000);
+  servo.write(180);
+  delay(2000);
+}
